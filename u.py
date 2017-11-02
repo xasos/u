ecf5850f444fd6d40af048c7eb9a156f27470eee
Stefan Aleksic <stefan@stefanaleksic.com>
2017-11-01 21:53:27 -0700

    Make code more pythonic

--- u.py
+++ u.py
@@ -1,31 +1,43 @@
+#!/usr/bin/python
 import validators
-import subprocess
-
+import os
+import sys
+  
 class U:
-    def generate_url(url, url_id):
-        validate_url(url)
-        check_id_availability(url_id)
+    def _get_file_path_for_url_id(self, url_id, file_name):
+      return '{}/{}'.format(url_id, file_name)
+   
+    def generate_url(self, url, url_id):
+        self.validate_url(url)
+        self.check_id_availability(url_id)
         # check git status, unclean state
         # create url
         # create folder, cp html file in that folder with format
-        subprocess.check_output(['mkdir ' + url_id + '&& cd ' + url_id])
-        subprocess.check_output(['echo "some data for the file" >> fileName']) # redirect code
-        # return url from constants file, open url, return url to CL
+        directory_name = url_id
+        # TODO: @xasos: implement file name logic
+        file_name = 'fileName'
+        if not os.path.exists(url_id):
+            os.makedirs(url_id)
+        file_path = self._get_file_path_for_url_id(url_id, file_name)
+        with open(file_path, 'w') as f:
+            # TODO: @xasos implement this to actually save real data
+            f.write('data')
   
-    def validate_url(url):
+        # return url from constants file, open url, return url to CL
+
+    def validate_url(self, url):
         if validators.url(url):
             return True
         else:
             raise ValueError('Invalid URL format. Please try again.')
   
-    def check_id_availability(url_id):
+    def check_id_availability(self, url_id):
         # check branch, git status, git push
         # write current dir
         # check 404?
-        subprocess.check_output(['[ -d "YOUR_DIR" ] && echo "is a dir"'])
-        pass
+        return not os.path.exists(url_id)
   
-    def update_all():
+    def update_all(self):
         #search directory
         #update all dir
         #update urls
@@ -36,7 +48,8 @@ if __name__ == "__main__":
     if (len(sys.argv) == 3):
         url = sys.argv[1]
         url_id = sys.argv[2]
-        generate_url(url, url_id)
+        u = U()
+        u.generate_url(url, url_id)
     else:
         raise ValueError('Invalid command. Try the following command\
                           format:\n \t u https://google.com google')
