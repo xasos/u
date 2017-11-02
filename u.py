c11d517d19f27140d9d629b3cf6f363fe818e1ee
Stefan Aleksic <stefan@stefanaleksic.com>
2017-11-01 21:39:12 -0700

    Fix lint issues.

--- u.py
+++ u.py
@@ -2,34 +2,35 @@ import validators
 import subprocess
 
 class U:
-  def generate_url(url, url_id):
-      validate_url(url)
-      check_id_availability(url_id)
-      # check git status, unclean state
-      # create url
-      # create folder, cp html file in that folder with format
-      subprocess.check_output(['mkdir ' + url_id + '&& cd ' + url_id])
-      subprocess.check_output(['echo "some data for the file" >> fileName']) # redirect code
-      # return url from constants file, open url, return url to CL
-
-  def validate_url(url):
-      if validators.url(url):
-          return True
-      else:
-          raise ValueError('Invalid URL format. Please try again.')
-
-  def check_id_availability(url_id):
-      # check branch, git status, git push
-      # write current dir
-      # check 404?
-      subprocess.check_output(['[ -d "YOUR_DIR" ] && echo "is a dir"'])
-      pass
-
-  def update_all():
-      #search directory
-      #update all dir
-      #update urls
-      #list urls
+    def generate_url(url, url_id):
+        validate_url(url)
+        check_id_availability(url_id)
+        # check git status, unclean state
+        # create url
+        # create folder, cp html file in that folder with format
+        subprocess.check_output(['mkdir ' + url_id + '&& cd ' + url_id])
+        subprocess.check_output(['echo "some data for the file" >> fileName']) # redirect code
+        # return url from constants file, open url, return url to CL
+  
+    def validate_url(url):
+        if validators.url(url):
+            return True
+        else:
+            raise ValueError('Invalid URL format. Please try again.')
+  
+    def check_id_availability(url_id):
+        # check branch, git status, git push
+        # write current dir
+        # check 404?
+        subprocess.check_output(['[ -d "YOUR_DIR" ] && echo "is a dir"'])
+        pass
+  
+    def update_all():
+        #search directory
+        #update all dir
+        #update urls
+        #list urls
+        pass
 
 if __name__ == "__main__":
     if (len(sys.argv) == 3):
@@ -37,5 +38,5 @@ if __name__ == "__main__":
         url_id = sys.argv[2]
         generate_url(url, url_id)
     else:
-        raise ValueError('Invalid command. Try the following command
+        raise ValueError('Invalid command. Try the following command\
                           format:\n \t u https://google.com google')
