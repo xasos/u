6db763e2bf85538f15ca026468d30725f0cf687b
Niraj Pant <nirajpant7@gmail.com>
2017-12-20 03:01:23 -0600

    Clean up

--- u.py
+++ u.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python
+
+from math import floor
+import string
 import validators
 import os
 import sys
 
 class U:
+    def __init__(self):
+        pass
+
     def _get_file_path_for_url_id(self, url_id, file_name):
       return '{}/{}'.format(url_id, file_name)
+
+    def toBase62(self, num, b = 62):
+        if b <= 0 or b > 62:
+            return 0
+        base = string.digits + string.lowercase + string.uppercase
+        r = num % b
+        res = base[r];
+        q = floor(num / b)
+        while q:
+            r = q % b
+            q = floor(q / b)
+            res = base[int(r)] + res
+        return res
+    
+    def toBase10(self, num, b = 62):
+        base = string.digits + string.lowercase + string.uppercase
+        limit = len(num)
+        res = 0
+        for i in xrange(limit):
+            res = b * res + base.find(num[i])
+        return res
 
     def generate_url(self, url, url_id):
         self.validate_url(url)
@@ -23,32 +50,24 @@ class U:
             # TODO: @xasos implement this to actually save real data
             f.write('data')
 
+    def generate_url(self, url, url_id):
+        validate_url(url)
+        check_id_availability(url_id)
+        # check git status, unclean state
+        # create url
+        # create folder, cp html file in that folder with format
+        subprocess.check_output(['mkdir ' + url_id + '&& cd ' + url_id])
+        subprocess.check_output(['echo "some data for the file" >> fileName']) # redirect code
         # return url from constants file, open url, return url to CL
-  def __init__():
-      pass
 
-  def generate_url(url, url_id):
-      validate_url(url)
-      check_id_availability(url_id)
-      # check git status, unclean state
-      # create url
-      # create folder, cp html file in that folder with format
-      subprocess.check_output(['mkdir ' + url_id + '&& cd ' + url_id])
-      subprocess.check_output(['echo "some data for the file" >> fileName']) # redirect code
-      # return url from constants file, open url, return url to CL
+    def create_file(self):
+        pass
 
-  def validate_url(url):
-      if validators.url(url):
-          return True
-      else:
-          raise ValueError('Invalid URL format. Please try again.')
+    def read_file(self, value):
+        pass
 
-  def check_id_availability(url_id):
-      # check branch, git status, git push
-      # write current dir
-      # check 404?
-      subprocess.check_output(['[ -d "YOUR_DIR" ] && echo "is a dir"'])
-      pass
+    def write_file(self, value):
+        pass
 
     def validate_url(self, url):
         if validators.url(url):
@@ -60,6 +79,17 @@ class U:
         # check branch, git status, git push
         # write current dir
         # check 404?
+        subprocess.check_output(['[ -d "YOUR_DIR" ] && echo "is a dir"'])
+        pass
+
+    def validate_url(self, url):
+        if validators.url(url):
+            return True
+        else:
+            raise ValueError('Invalid URL. Please try again.')
+
+    def check_id_availability(self, url_id):
+        # check file
         return not os.path.exists(url_id)
 
     def update_all(self):
@@ -71,11 +101,11 @@ class U:
 
 if __name__ == "__main__":
     u_client = U()
-    if (len(sys.argv) == 3):
-        url = sys.argv[1]
-        url_id = sys.argv[2]
-        u = U()
-        u.generate_url(url, url_id)
-    else:
-        raise ValueError('Invalid command. Try the following command\
-                          format:\n \t u https://google.com google')
+    #if (len(sys.argv) == 3):
+    #    url = sys.argv[1]
+    #    url_id = sys.argv[2]
+    #    u = U()
+    #    u.generate_url(url, url_id)
+    #else:
+    #    raise ValueError('Invalid command. Try the following command\
+    #                      format:\n \t u https://google.com google')
